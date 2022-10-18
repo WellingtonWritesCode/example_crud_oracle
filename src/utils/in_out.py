c1afd3902bdc855325f543870c21c9efb81fa4db
Wellington <17155519+WellingtonWritesCode@users.noreply.github.com>
2022-10-17 22:37:01 -0300

    Update in_out.py

--- src/utils/in_out.py
+++ src/utils/in_out.py
@@ -1,25 +1,6 @@
 import PySimpleGUI as sg
 import re
 
-def le_int(texto:str="Insira um numero:") -> int:
-
-    window = sg.Window("Entrada", [[sg.T(texto)],
-                                   [sg.Input(s=(20, 1), key='-IN-'), sg.B('OK', key='-OK-')]])
-
-    while True:
-
-        event, values = window.read()
-
-        if event == sg.WINDOW_CLOSED:
-            return 0
-        elif event == '-OK-':
-            try:
-                out = int(values['-IN-'])
-                window.close()
-                return out
-            except ValueError:
-                sg.PopupOK("Numero invalido!")
-
 def le_cpf(texto: str = ""):
 
     window = sg.Window("Entrada", [[sg.T(texto)],
