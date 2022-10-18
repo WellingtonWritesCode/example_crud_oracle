f5257e92797931aa5a83fe55099c1afe66047a2c
Wellington <17155519+WellingtonWritesCode@users.noreply.github.com>
2022-10-17 22:27:55 -0300

    Update relatorios.py

--- src/reports/relatorios.py
+++ src/reports/relatorios.py
@@ -3,13 +3,13 @@ from conexion.oracle_queries import OracleQueries
 
 class Relatorio:
     def __init__(self):
-        with open("src/sql/relatorio_socios.sql") as f:
+        with open("../src/sql/relatorio_socios.sql") as f:
             self.query_relatorio_socios = f.read()
 
-        with open("src/sql/relatorio_planos.sql") as f:
+        with open("../src/sql/relatorio_planos.sql") as f:
             self.query_relatorio_planos = f.read()
 
-        with open("src/sql/relatorio_socios_por_plano.sql") as f:
+        with open("../src/sql/relatorio_socios_por_plano.sql") as f:
             self.query_relatorio_socios_por_plano = f.read()
 
     def get_relatorio_socios(self) -> DataFrame:
